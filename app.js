53c97d1e18f2425c8467d4350a677fe787447cde
jinshuilinxi <2329135071@qq.com>
2020-04-10 10:18:32 +0800

    处理URL

--- app.js
+++ app.js
@@ -2,23 +2,10 @@ const Koa = require('koa')
 
 const app = new Koa()
 
-app.use(async (ctx, next) => {
-  console.log(`${ctx.request.method} ${ctx.request.url}`)
-  await next()
-})
-
-app.use(async (ctx, next) => {
-  const start = new Date().getTime()
-  await next()
-  const ms = new Date().getTime() - start
-  console.log(`${ms}ms`) // 耗费时间
-})
-
-app.use(async (ctx, next) => {
-  await next()
-  ctx.response.type = 'text/html'
-  ctx.response.body = '<p>hello hello hey</p>'
-})
+const bodyPaeser = require('koa-bodyparser')
+const controller = require('./controller');
+app.use(bodyPaeser())
+app.use(controller())
 
 app.listen(3000)
 console.log('app startes at port 3000..')
